76f85390b161277f1d066c2b560f24afa136e0c1
IagoAntunes <iagoantunes.f@gmail.com>
2020-10-08 01:31:34 -0300

    Iago Antunes Ferreira

---
+++ CursoEmVideo/Aula12/exercicio011.js
@@ -0,0 +1,14 @@
+var idade = 37
+if(idade <16){
+    console.log('Não Vota')
+}
+else{
+    if(idade < 18 || idade > 67){
+        console.log('Voto Opcional')
+    }
+    else{
+        console.log("Voto Obrigatorio")
+    }
+
+
+}
