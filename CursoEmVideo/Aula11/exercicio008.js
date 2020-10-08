599cbfa54a6bb871e92f178a9c4fc794bdb2ef3f
IagoAntunes <iagoantunes.f@gmail.com>
2020-10-07 22:03:09 -0300

    Iago Antunes Ferreira

---
+++ CursoEmVideo/Aula11/exercicio008.js
@@ -0,0 +1,9 @@
+
+var vel = 60.5
+
+if(vel > 40){
+    console.log(`Você ultrapassou a velocidade permitida`)
+}else{
+    console.log(`Você esta limpo`)
+}
+console.log(`Velocidade =  ${vel}`)
---
+++ CursoEmVideo/Aula11/exercicio009.js
@@ -0,0 +1,8 @@
+pais = 'BRASIL'
+
+if(pais == 'Brasil'.toUpperCase){
+    console.log(`Brasilero`)
+}else{
+    console.log(`Estrangeiro`)
+}
+console.log(`Você é do ${pais}`)
